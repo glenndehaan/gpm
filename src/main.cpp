e45291307ccfd2db8133ca61fc37a4380560f1c3
Glenn de Haan <glenn@dehaan.cloud>
2019-08-05 09:21:32 +0200

    Added console colors and debug message/mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 
+// Define global variables
 std::string version = "v0.0.0";
+bool debug = true;
 
+// Define colors
+const std::string colorRed("\033[0;31m");
+const std::string colorGreen("\033[0;32m");
+const std::string colorYellow("\033[0;33m");
+const std::string colorBlue("\033[0;34m");
+const std::string colorMagenta("\033[0;35m");
+const std::string colorCyan("\033[0;36m");
+const std::string colorWhite("\033[0;37m");
+const std::string colorReset("\033[0m");
+
+// Help fallback function
 void help() {
-    std::cout << "GPM (Glenn Package Manager) ";
-    std::cout << version;
-    std::cout << "\n";
+    std::cout << "GPM (Glenn Package Manager) " << version << "\n";
     std::cout << "Usage: gpm [OPTIONS] install|remove pkg1 [pkg2 ...]\n\n";
     std::cout << "Option               Meaning\n";
     std::cout << "-h, --help           Shows this message\n";
 }
 
+// Main function
 int main(int argc, char* argv[]) {
+    if(debug) {
+        std::cout << colorYellow << "!!!! WARNING: DEBUG ENABLED!!!!\n\n" << colorReset;
+    }
+
     if(argc < 2) {
         help();
     } else {
